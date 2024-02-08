daa3de73c4b4d209f662fac54b1e7c8650dddb3d
Canaga81 <Canagamemmedov@gmail.com>
2024-02-08 13:38:19 +0400

    Menus Data

--- src/components/tree-view/data.jsx
+++ src/components/tree-view/data.jsx
@@ -4,56 +4,92 @@ export const menus = [
     to: "/",
   },
   {
-    label: "Profile",
-    to: "/profile",
+    label: "About",
+    to: "/about",
+  },
+  {
+    label: "Companines",
+    to: "/companines",
     children: [
       {
-        label: "Details",
-        to: "details",
+        label: "Yusuf-UM MMC",
+        to: "/yusuf-um mmc",
         children: [
           {
-            label: "Location",
-            to: "location",
-            children: [
-              {
-                label: "City",
-                to: "city",
-              },
-            ],
+            label: "Su əsaslı  boyalar",
+            to: "/suəsaslıboyalar",
+          },
+          {
+            label: "Sellozik boyalar",
+            to: "/sellozikboyalar",
+          },
+          {
+            label: "Hammenton boyalar",
+            to: "/hammentonboyalar",
+          },
+          {
+            label: "Sintetik boyalar",
+            to: "/sintetikboyalar",
+          },
+          {
+            label: "Sənaye boyaları",
+            to: "/sənayeboyaları",
+          },
+          {
+            label: "Sellozik ve Sintetik tiner",
+            to: "/sellozikvesintetiktiner",
+          },
+        ],
+      },
+      {
+        label: "Unity MMC",
+        to: "/unit mmc",
+        children: [
+          {
+            label: "Maliyyə və Mühasibatlıq",
+            to: "/maliyyə və mühasibatlıq",
+          },
+          {
+            label: "Vergi Audit ",
+            to: "/vergi audit",
+          },
+          {
+            label: "Gömrük rəsmiləşdirilməsi və Broker xidmətləri",
+            to: "/gömrük rəsmiləşdirilməsi və broker xidmətləri",
+          },
+          {
+            label: "Dizayn Xidmətləri",
+            to: "/dizayn xidmətləri",
+          },
+        ],
+      },
+      {
+        label: "EH Trade",
+        to: "/eh trade",
+        children: [
+          {
+            label: "Toz boya məhsullar",
+            to: "/tozboya məhsullar",
+          },
+          {
+            label: "Zımpara və Sumpata kağızı ",
+            to: "/zımparavəsumpatakağızı",
+          },
+          {
+            label: "Boya ruloları və fırçaları",
+            to: "/boyarulolarıvəfırçaları",
+          },
+          {
+            label: "Xırdavatlar",
+            to: "/xırdavatlar",
           },
         ],
       },
     ],
   },
   {
-    label: "Settings",
-    to: "/settings",
-    children: [
-      {
-        label: "Account",
-        to: "account",
-      },
-      {
-        label: "Security",
-        to: "security",
-        children: [
-          {
-            label: "Login",
-            to: "login",
-          },
-          {
-            label: "Register",
-            to: "register",
-            children: [
-              {
-                label: "Random data",
-                to: "",
-              },
-            ],
-          },
-        ],
-      },
-    ],
+    label: "Contact",
+    to: "/contact",
   },
 ];
 
